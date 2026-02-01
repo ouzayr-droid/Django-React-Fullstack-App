6bffe5a14422ab69ed5317190a30b0316ed8825e
ouzayr-droid <salifououzerou25@gmail.com>
2026-02-01 04:49:43 +0100

    feature: ajout d'un formulaire générique (login-register)

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import api from "../api.js";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constant.js";
+
+function Form({route, method}){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const name = method === "login" ? "Login" : "Register";
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+
+        try {
+            const res = await api.post(route, {username, password});
+            if(res.status === 200 || res.status === 201){
+                if(method === "login"){
+                    localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                    localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                    navigate("/");
+                }else{
+                    navigate("/login");
+                }
+        } catch (error) {
+            alert("An error occurred. Please try again.");
+            console.error("Form submission error:", error);
+        }finally{
+            setLoading(false);
+        }
+    }
+    return <form onSubmit={handleSubmit} className="form-container">
+        <h1>{name}</h1>
+        <input type="text" className="form-input" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+        <input type="password" className="form-input" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <button type="submit" className="form-button" disabled={loading}>
+            {loading ? "Please wait..." : name}
+        </button>
+    </form>;
+}
